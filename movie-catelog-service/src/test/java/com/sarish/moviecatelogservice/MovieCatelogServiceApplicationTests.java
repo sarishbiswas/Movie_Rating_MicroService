502e3ed057664ac7bc34c1b4ccae08e6f67d75f2
sarishbiswas <sarishbiswas@gmail.com>
2020-09-07 21:39:38 +0530

    Initial commit of the Microservice Application

---
+++ movie-catelog-service/src/test/java/com/sarish/moviecatelogservice/MovieCatelogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarish.moviecatelogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatelogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

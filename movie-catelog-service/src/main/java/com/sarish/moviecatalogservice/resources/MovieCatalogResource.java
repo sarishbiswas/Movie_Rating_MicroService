900ffea75546270b5a5420f77b9cee8210b7dbbd
sarishbiswas <sarishbiswas@gmail.com>
2020-09-10 22:46:33 +0530

    Using WebClient.Builder Making an API call To Microservices

--- movie-catelog-service/src/main/java/com/sarish/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catelog-service/src/main/java/com/sarish/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,19 +1,45 @@
 package com.sarish.moviecatalogservice.resources;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.sarish.moviecatalogservice.models.CatalogItem;
+import com.sarish.moviecatalogservice.models.Movie;
+import com.sarish.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+	@Autowired
+	private RestTemplate restTemplate;
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		return Collections.singletonList(new CatalogItem("Anindita","M Girl",5));
+		
+		
+		//Get all rated movie Id
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1",4),new Rating("2",5)
+			);
+		//for each movie id call  movie-info-service and get details
+		return ratings.stream().map(rating->{
+//			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie = webClientBuilder.build()
+											.get()
+											.uri("http://localhost:8082/movies/"+rating.getMovieId())
+											.retrieve()
+											.bodyToMono(Movie.class)
+											.block();
+			return new CatalogItem(movie.getName(),"M Girl",rating.getRating());
+			}).collect(Collectors.toList());
 	}
 }
